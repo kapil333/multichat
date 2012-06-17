fa1a409ff54614b82208cc5c37dd0033fa604abb
raphael b <raphaelbaron@gmail.com>
2012-06-17 15:26:25 +0200

    Typo on mythread.cpp.

--- src/mythread.cpp
+++ src/mythread.cpp
@@ -19,7 +19,7 @@ int MyThread::Create(void *Callback, void *args) {
     return tret;
   }
   else {
-    cout << "Thread succesfuly created." << endl;
+    cout << "Thread successfuly created." << endl;
     return 0;
   }
 }
